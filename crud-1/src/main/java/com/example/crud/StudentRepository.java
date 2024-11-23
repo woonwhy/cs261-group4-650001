fad49e427c7dfaf146e5f608e5d945f7a1464cd2
Creampiraya <piraya.en-@dome.tu.ac.th>
2024-11-24 02:58:20 +0700

    add form4.js

--- crud-1/src/main/java/com/example/crud/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.crud;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-    // คุณสามารถเพิ่มเมธอดค้นหาข้อมูลเพิ่มเติมได้ตามต้องการ
-}
