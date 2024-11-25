34602b853f8590797e2a7c523ed5b70609ef6366
Muayminly <145211564+Muayminly@users.noreply.github.com>
2024-11-25 17:58:16 +0700

    Email Sent

---
+++ crud-1/src/main/java/com/example/crud/EmailService.java
@@ -0,0 +1,27 @@
+package com.example.crud;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmailService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    public void sendEmails(List<String> emailAddresses, String subject, String body) {
+        for (String email : emailAddresses) {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setTo(email);
+            message.setSubject(subject);
+            message.setText(body);
+            mailSender.send(message);
+        }
+    }
+}
+
+
--- crud-1/src/main/java/com/example/crud/Student.java
+++ crud-1/src/main/java/com/example/crud/Student.java
@@ -16,6 +16,7 @@ public class Student {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+<<<<<<< Updated upstream
 	private Long id ; 
 	
 	@Column(name = "eng_name", nullable = false)
@@ -34,3 +35,48 @@ public class Student {
 	private String user_name ; 
 	
 }
+=======
+	private Long id ;
+	
+	@Column(name = "date", nullable = false)
+	private String date ;
+	
+	@Column(name = "thai_name", nullable = false)
+	private String displayname_th ;
+	
+	@Column(name = "user_name", nullable = false)
+	private String userName ; 
+	
+	@Column(name = "email", nullable = false)
+	private String email ;
+	
+	@Column(name = "faculty", nullable = false)
+	private String faculty ;
+	
+	@Column(name = "status", nullable = false)
+	private String status ;
+	
+	@Column(name = "address", nullable = false)
+	private String address ;
+	
+	@Column(name = "semester", nullable = false)
+	private String semester ;
+	
+	@Column(name = "courseCode", nullable = false)
+	private String courseCode ;
+	
+	@Column(name = "courseName", nullable = false)
+	private String courseName ;
+	
+	@Column(name = "section", nullable = false)
+	private String section ;
+	
+	@Column(name = "reason", nullable = false)
+	private String reason ;
+
+	public String getEmail() {
+		return email;
+	}
+
+}
+>>>>>>> Stashed changes
