bcfebe06afcd191580d264e22847e614287c7b1f
Chayada <chayada.thum@dome.tu.ac.th>
2024-11-26 10:51:39 +0700

    Pop-up when sent form(exoectform4)

--- FrontEnd/public/js/form1.js
+++ FrontEnd/public/js/form1.js
@@ -1,4 +1,18 @@
 window.onload = function () {
+    // ดึงข้อมูลผู้ใช้จาก Local Storage
+    const form1 = JSON.parse(localStorage.getItem('form1'));
+
+    if (form1) {
+        // กำหนดค่าต่าง ๆ ในฟอร์ม
+        document.getElementById('registrationDate').value = new Date().toLocaleDateString(); // วันที่จดทะเบียน (Auto fill)
+        document.getElementById('displayname_th').value = form1.displayname_th;
+        document.getElementById('username').value = form1.username;
+        document.getElementById('email').value = form1.email;
+        document.getElementById('faculty').value = form1.faculty;
+        document.getElementById('department').value = form1.department;
+        document.getElementById('tu_status').value = form1.tu_status;
+    }
+
     // เพิ่ม Event Listener สำหรับการ submit form
     document.querySelector('button[type="submit"]').addEventListener('click', function (e) {
         e.preventDefault(); // ป้องกันการ submit form แบบปกติ
